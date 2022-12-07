3e4ab9743a8da0f30d52dfe7052c998d6da72e3c
Mohammed Alam <ma3655@rit.edu>
2022-12-07 01:46:36 -0500

    Added Pause, Resume and Quit Functionality

--- legionrevenant.py
+++ legionrevenant.py
@@ -1,28 +1,53 @@
 from pynput.keyboard import Key, Controller
 import time
+import keyboard
 
-keyboard = Controller()
+pynputKeyBoard = Controller()
 sleepCount = 0.025
+
+
+def ComboSet():
+    time.sleep(sleepCount)
+    pynputKeyBoard.press("4")
+    pynputKeyBoard.release("4")
+
+    time.sleep(sleepCount)
+    pynputKeyBoard.press("5")
+    pynputKeyBoard.release("5")
+
+    time.sleep(sleepCount)
+    pynputKeyBoard.press("2")
+    pynputKeyBoard.release("2")
+
+    time.sleep(sleepCount)
+    pynputKeyBoard.press("3")
+    pynputKeyBoard.release("3")
+
+
+def ConstantRunning():
+    while True:
+
+        ComboSet()
+
+        if keyboard.is_pressed('p'):
+            print('Paused the Bot!')
+            break  # finishing the loop
 
 
 def main():
 
+    # Amount of time needed to click back into AQW
+    time.sleep(1)
+
+    ConstantRunning()
+
     while True:
-        time.sleep(sleepCount)
-        keyboard.press("4")
-        keyboard.release("4")
-
-        time.sleep(sleepCount)
-        keyboard.press("5")
-        keyboard.release("5")
-
-        time.sleep(sleepCount)
-        keyboard.press("2")
-        keyboard.release("2")
-
-        time.sleep(sleepCount)
-        keyboard.press("3")
-        keyboard.release("3")
+        if keyboard.is_pressed('r'):
+            print("Bot Resumed!")
+            ConstantRunning()
+        elif keyboard.is_pressed('q'):
+            print("Bot Ended!")
+            break
 
 
 main()
