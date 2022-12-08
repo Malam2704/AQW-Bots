46695d8f34a66ce256a89a42902e49d9dd71c214
Mohammed Alam <ma3655@rit.edu>
2022-12-08 16:44:41 -0500

    commented code

--- legionrevenant.py
+++ legionrevenant.py
@@ -2,11 +2,16 @@ from pynput.keyboard import Key, Controller
 import time
 import keyboard
 
+# Constants: the pynputlibrary and the constant wait
+# time between each move/keyboard press
 pynputKeyBoard = Controller()
 sleepCount = 0.025
 
 
 def ComboSet():
+    # The combo for each class, change for
+    # other classes
+
     time.sleep(sleepCount)
     pynputKeyBoard.press("4")
     pynputKeyBoard.release("4")
@@ -25,8 +30,10 @@ def ComboSet():
 
 
 def ConstantRunning():
-    while True:
+    # Loop running combo function abvoe and waiting
+    # pause
 
+    while True:
         ComboSet()
 
         if keyboard.is_pressed('p'):
@@ -39,8 +46,11 @@ def main():
     # Amount of time needed to click back into AQW
     time.sleep(1)
 
+    # The function to loop a combo and pause the loop
     ConstantRunning()
 
+    # If a user paused this loop pops up waiting for a player
+    # to resume to continue
     while True:
         if keyboard.is_pressed('`'):
             print("Bot Resumed!")
